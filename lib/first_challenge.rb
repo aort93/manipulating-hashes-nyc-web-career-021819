93984033a06706e818c657f91eb68166993c5913
Alonso Ortega <alonsojortega@gmail.com>
2019-01-31 02:57:07 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -2,7 +2,7 @@ def first_challenge
   contacts = {
     "Jon Snow" => {
       name: "Jon",
-      email: "jon_snow@thewall.we", 
+      email: "jon_snow@thewall.we",
       favorite_icecream_flavors: ["chocolate", "vanilla", "mint chip"],
       knows: nil
     },
@@ -14,7 +14,7 @@ def first_challenge
   }
 
   #your code here
-  
+
   contacts['Freddy Mercury'].each do |key, val|
     if key == :favorite_icecream_flavors
       value.delete_if do |flavor|
@@ -26,4 +26,3 @@ def first_challenge
   #remember to return your newly altered contacts hash!
   contacts
 end
-
